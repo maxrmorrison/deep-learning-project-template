3a25e85622f62a8f9fcc95ddb9f12fc314151378
Max Morrison <maxrmorrison@gmail.com>
2022-10-24 21:53:52 -0500

    Update __main__.py

--- NAME/preprocess/__main__.py
+++ NAME/preprocess/__main__.py
@@ -21,5 +21,4 @@ def parse_args():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
-    NAME.preprocess.datasets(**vars(parse_args()))
+NAME.preprocess.datasets(**vars(parse_args()))
